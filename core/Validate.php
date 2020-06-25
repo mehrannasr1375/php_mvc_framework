07f9d95fcdc5d21dbc0b86bd7b1e54da8d50e6e8
mehrannasr1375 <mrparadox1397@gmail.com>
2020-06-25 14:08:20 +0430

    fix jquery error

--- core/Validate.php
+++ core/Validate.php
@@ -132,7 +132,7 @@ class Validate {
         foreach ($this->errors as $error) {
             if (is_array($error)) {
                 $html .= '<li class="text-danger">'.$error[0].'</li>';
-                $html .= '<script>jQuery("document).ready(function() {jQuery("#'.$error[1].'").parent().closest("div").addClass("has-error");})</script>';
+                $html .= '<script>jQuery("document").ready(function(){jQuery("#'.$error[1].'").parent().closest("div").addClass("has-error");})</script>';
             } 
             else {
                 $html .= '<li class="text-danger">'.$error.'</li>';
